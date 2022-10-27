dcfca4ba913b9593c013e30828a5225acf9b8c46
David <davidnovo1999@gmail.com>
2022-10-27 10:48:29 +0200

    Initial commit

--- app/src/main/java/com/example/fundamentals/MainActivity.java
+++ app/src/main/java/com/example/fundamentals/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.fundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button cancel;
+    EditText edituser,editpassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        cancel=findViewById(R.id.cancel);
+        edituser=findViewById(R.id.edituser);
+        editpassword=findViewById(R.id.editpassword);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
