4fb785227551679463ba897fcf79b5a63d7d443f
David <davidnovo1999@gmail.com>
2022-11-02 10:47:43 +0100

    Initial commit

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -2,24 +2,34 @@ package com.example.fundamentals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button cancel;
-    EditText edituser,editpassword;
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        cancel=findViewById(R.id.cancel);
-        edituser=findViewById(R.id.edituser);
-        editpassword=findViewById(R.id.editpassword);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signup=findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(i);
+
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/fundamentals/SignupActivity.java
+++ app/src/main/java/com/example/fundamentals/SignupActivity.java
@@ -3,12 +3,22 @@ package com.example.fundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
 
+   EditText edituser, editpassword, editemail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        edituser = findViewById(R.id.edituser);
+        editpassword = findViewById(R.id.editpassword);
+        editemail = findViewById(R.id.editemail);
+
     }
 }
