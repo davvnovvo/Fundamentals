feb709f45a87c8cbb7367788fc8f50f7a4c3ae66
David <davidnovo1999@gmail.com>
2022-11-03 10:13:38 +0100

    Initial commit

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView signup;
+    Button cancel;
+    EditText edituser, editpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,9 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         signup = findViewById(R.id.signup);
+        cancel = findViewById(R.id.cancel);
+        edituser = findViewById(R.id.edituser);
+        editpassword = findViewById(R.id.editpassword);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +34,16 @@ public class LoginActivity extends AppCompatActivity {
 
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                edituser.setText("");
+                editpassword.setText("");
+
             }
         });
 
