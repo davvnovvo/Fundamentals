2f475f6eb9800c6a9d076bad4196da07603f8cde
David <davidnovo1999@gmail.com>
2022-11-03 10:18:18 +0100

    Initial commit

--- app/src/main/java/com/example/fundamentals/SignupActivity.java
+++ app/src/main/java/com/example/fundamentals/SignupActivity.java
@@ -3,12 +3,15 @@ package com.example.fundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
 
-    EditText edituser, editpassword, editemail;
+    EditText edituser, editpassword, editpassword2, editemail;
+    Button cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +19,23 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        cancel = findViewById(R.id.cancel);
         edituser = findViewById(R.id.edituser);
         editpassword = findViewById(R.id.editpassword);
         editemail = findViewById(R.id.editemail);
+        editpassword2 = findViewById(R.id.editpassword2);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                edituser.setText("");
+                editpassword.setText("");
+                editpassword2.setText("");
+                editemail.setText("");
+
+            }
+        });
 
     }
 }
