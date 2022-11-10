5f737f323fe122c98cb91105757e59e33462beeb
David <davidnovo1999@gmail.com>
2022-11-10 10:40:57 +0100

    Initial commit

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -3,11 +3,16 @@ package com.example.fundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +25,18 @@ public class LoginActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//    Glide for loading girls
+        ImageView mFondo = findViewById(R.id.fondo);
+        Glide.with(this)
+                .load("https://i.pinimg.com/736x/4c/30/34/4c3034861b40f0fb4c0b3b5fbefe77e9.jpg")
+//                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+
+                .centerCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.orange)))
+//                .circleCrop()
+                .into(mFondo);
 
         signup = findViewById(R.id.signup);
         cancel = findViewById(R.id.cancel);
