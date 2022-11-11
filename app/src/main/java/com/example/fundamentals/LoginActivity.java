495a0313d9f6517c29cfba0384854c2a72d97f0a
David <davidnovo1999@gmail.com>
2022-11-11 09:09:52 +0100

    Initial commit

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -19,6 +19,7 @@ public class LoginActivity extends AppCompatActivity {
     TextView signup;
     Button cancel;
     EditText edituser, editpassword;
+    ImageView mFondo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +27,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//    Glide for loading girls
-        ImageView mFondo = findViewById(R.id.fondo);
-        Glide.with(this)
-                .load("https://i.pinimg.com/736x/4c/30/34/4c3034861b40f0fb4c0b3b5fbefe77e9.jpg")
-//                .load(R.drawable.girl)
-                .transition(DrawableTransitionOptions.withCrossFade(100))
-
-                .centerCrop()
-//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.orange)))
-//                .circleCrop()
-                .into(mFondo);
-
         signup = findViewById(R.id.signup);
         cancel = findViewById(R.id.cancel);
         edituser = findViewById(R.id.edituser);
         editpassword = findViewById(R.id.editpassword);
+        mFondo = findViewById(R.id.fondo);
+
+        Glide.with(this)
+                .load("https://i.pinimg.com/736x/4c/30/34/4c3034861b40f0fb4c0b3b5fbefe77e9.jpg")
+                .into(mFondo);
+
+        /*Glide.with(this)
+                .load("https://i.pinimg.com/736x/4c/30/34/4c3034861b40f0fb4c0b3b5fbefe77e9.jpg")
+                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.orange)))
+                .circleCrop()
+                .into(mFondo);*/
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
