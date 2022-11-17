f301ccc1731e432589cefca2dba45bdf982222a3
David <davidnovo1999@gmail.com>
2022-11-17 09:54:26 +0100

    Initial commit

---
+++ app/src/main/java/com/dnovo/first/Splash.java
@@ -0,0 +1,66 @@
+package com.dnovo.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.example.fundamentals.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        openApp();
+
+
+        //implements and starts animation
+        // objeto thunder sobre el cual aplicaremos la animación
+        ImageView thunder = findViewById(R.id.logosplash);
+
+        //creamos un objeto animación que incorpora la animación descrita en el xml y con el método
+        // startAnimation lo aplicamos al imageview del logo
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        thunder.startAnimation(myanim);
+
+
+        //    Glide for loading girls
+        ImageView mFondo = findViewById(R.id.backView);
+
+        Glide.with(this)
+//                .load("https://img.freepik.com/vector-gratis/fondo-maravilloso-psicodelico-dibujado-mano-plana_23-2148850121.jpg?w=2000")
+                .load(R.drawable.fondo2)
+                .transition(DrawableTransitionOptions.withCrossFade(200))
+                .centerCrop()
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+//                .circleCrop()
+                .into(mFondo);
+
+    }
+
+    private void openApp() {
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash
+                        .this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 5000);
+
+
+    }
+
+}
